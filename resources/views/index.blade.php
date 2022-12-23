e5710e433937449557477ed44bd0736c5b8d2fed
kengokonishi0824 <konishikengo0824@gamail.com>
2022-12-23 22:13:19 +0900

    define_like_function #11

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -41,20 +41,19 @@
         <a class="btn_detail" href="/detail/{{$restaurant->id}}">
           詳しく見る
         </a>
-        @if($restaurant->like->first() != null)
-        <a href="{{route('unlike', $restaurant)}}" class=btn-like>
-          あかん
+        @if($likes->where('restaurant_id', $restaurant->id)->where('user_id', auth()->user()->id)->first() != null)
+        <a href="{{route('unlike', $restaurant)}}">
+          <img src="{{asset('/picture/heart_fill.png')}}" alt="" class=btn-like>
         </a>
         @else
-        <a href="{{route('like', $restaurant)}}" class=btn-like>
-          いいね
+        <a href="{{route('like', $restaurant)}}">
+          <img src="{{asset('/picture/heart.png')}}" alt="" class=btn-like>
         </a>
         @endif
       </div>
   </div>
   @endforeach
 </div>
-
 
 
 

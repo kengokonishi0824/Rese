b5e97099d73370ebf8224ef3c56cc96339ac51cb
kengokonishi0824 <konishikengo0824@gamail.com>
2022-12-28 20:10:12 +0900

    html_arrangemenat

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -6,15 +6,10 @@
   <title>my_page</title>
 </head>
 
-@if (Auth::check())
-  <p>ログイン中ユーザー: {{$user->id}}</p>
-@else
-  <p>ログインなし。（<a href="/login">ログイン</a>｜
-  <a href="/register">登録</a>）</p>
-@endif
-
 <div class="mypage">
   <div class="mypage-left">
+    <p class="mypage-name">Rese</p>
+    <p class="mypage-subtitle">予約状況</p>
     @foreach ($reservations as $reservation)
     <div class="reservation-all">
       <p class="confirm-content">{{$reservation->restaurant->name}}</p>
@@ -26,29 +21,38 @@
   </div>
 
   <div class="mypage-right">
-    @foreach ($likes as $like)
-    <div class="restaurant">
-      <img src="{{$like->restaurant->picture}}" class="picture">
-      <p class="restaurant_name">{{$like->restaurant->name}}</p>
-      <p class="restaurant_tag">#{{$like->restaurant->prefecture->prefecture}} #{{$like->restaurant->category->category}}</p>
-      <div class="links">
-        <a class="btn_detail" href="/detail/{{$like->restaurant->id}}">
+    @if (Auth::check())
+    <p class="mypage-name">{{$user->name}}　さん</p>
+    @else
+    <p>ログインなし。（<a href="/login">ログイン</a>｜
+    <a href="/register">登録</a>）</p>
+    @endif
+    <p class="mypage-subtitle">お気に入り店舗</p>
+    <div class="mypage-kike-all">
+      @foreach ($likes as $like)
+      <div class="mypage-reservation">
+        <img src="{{$like->restaurant->picture}}" class="picture">
+        <p class="restaurant_name">{{$like->restaurant->name}}</p>
+        <p class="restaurant_tag">#{{$like->restaurant->prefecture->prefecture}} #{{$like->restaurant->category->category}}</p>
+        <div class="links">
+          <a class="btn_detail" href="/detail/{{$like->restaurant->id}}">
           詳しく見る
-        </a>
-        @if($like->where('restaurant_id', $like->restaurant->id)->where('user_id', auth()->user()->id)->first() != null)
-        <a href="{{route('unlike', $like->restaurant_id)}}">
-          <img src="{{asset('/picture/heart_fill.png')}}" alt="" class=btn-like>
-        </a>
-        @else
-        <a href="{{route('like', $like->restaurant_id)}}">
-          <img src="{{asset('/picture/heart.png')}}" alt="" class=btn-like>
-        </a>
-        @endif
+          </a>
+          @if($like->where('restaurant_id', $like->restaurant->id)->where('user_id', auth()->user()->id)->first() != null)
+          <a href="{{route('unlike', $like->restaurant_id)}}">
+            <img src="{{asset('/picture/heart_fill.png')}}" alt="" class=btn-like>
+          </a>
+          @else
+          <a href="{{route('like', $like->restaurant_id)}}">
+            <img src="{{asset('/picture/heart.png')}}" alt="" class=btn-like>
+          </a>
+          @endif
       </div>
     </div>
     @endforeach
   </div>
 </div>
+
 
 
 

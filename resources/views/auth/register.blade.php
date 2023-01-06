922b5d5478440aacd7a374241b7e868011058438
kengokonishi0824 <konishikengo0824@gamail.com>
2023-01-06 20:19:54 +0900

    login_page #23

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,10 +1,10 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="{{ asset('/css/style.css')  }}" >
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>register</title>
+</head>
 
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
@@ -35,16 +35,7 @@
                                 name="password"
                                 required autocomplete="new-password" />
             </div>
-
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
-            </div>
-
+            
             <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
                     {{ __('Already registered?') }}
@@ -55,5 +46,3 @@
                 </x-button>
             </div>
         </form>
-    </x-auth-card>
-</x-guest-layout>

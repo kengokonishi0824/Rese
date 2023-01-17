096c5e729748d7c736f7f468e8821d14ca31bab7
kengokonishi0824 <konishikengo0824@gamail.com>
2023-01-17 15:07:18 +0900

    controller #40

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function adminLogin(Request $request)
+    {
+        $credentials = $request->validate([ // 入力内容のチェック
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::guard('admin')->attempt($credentials)) { // ログイン試行
+            if ($request->user('admin')?->admin_level > 0) { // 管理権限レベルが0でないか
+                $request->session()->regenerate(); // セッション更新
+
+                return redirect()->intended('admin/dashboard'); // ダッシュボードへ
+            } else {
+                Auth::guard('admin')->logout(); // if文でログインしてしまっているので、ログアウトさせる
+
+                $request->session()->regenerate(); // セッション更新
+
+                return back()->withErrors([ // 権限レベルが0の場合
+                    'error' => 'You do not have permission to log in.',
+                ]);
+            }
+        }
+
+        return back()->withErrors([ // ログインに失敗した場合
+            'error' => 'The provided credentials do not match our records.',
+        ]);
+    }
+}

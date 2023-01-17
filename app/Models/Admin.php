e9f0fa8d42958f2533436fd18322d50846c0f674
kengokonishi0824 <konishikengo0824@gamail.com>
2023-01-17 14:52:01 +0900

    model_config #40

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -5,7 +5,25 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Admin extends Model
+class Admin extends Authenticatable
 {
     use HasFactory;
+
+    protected $table = 'admin_users';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'admin_level',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 }

e1f4a11445063029822231fb7393c80bf9ec9060
kengokonishi0824 <konishikengo0824@gamail.com>
2023-01-19 15:46:28 +0900

    seeder #39

--- database/seeders/AdminsTableSeeder.php
+++ database/seeders/AdminsTableSeeder.php
@@ -32,5 +32,14 @@ class AdminsTableseeder extends Seeder
             'admin_level' => 0
         ];
         Admin::create($param);
+
+        $param = [
+            'name' => '仙人マネジャー',
+            'email' => 'manger@example.com',
+            'password' => Hash::make('password'),
+            'VPN' => '1',
+            'admin_level' => 2
+        ];
+        Admin::create($param);
     }
 }
